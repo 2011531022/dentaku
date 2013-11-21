691b77ed54ce056f8e347f26116dcf92d66876a3
Tatsuya Kmagae <s1531022@eng.kitakyu-u.ac.jp>
2013-11-21 15:17:52 +0900

    labelとボタンの関連づけ

--- dentaku/ViewController.h
+++ dentaku/ViewController.h
@@ -8,7 +8,50 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController :
+    UIViewController{
+        IBOutlet UIButton *clearbtn;
+        IBOutlet UIButton *zerobtn;
+        IBOutlet UIButton *itibtn;
+        IBOutlet UIButton *nibtn;
+        IBOutlet UIButton *sanbtn;
+        IBOutlet UIButton *yonbtn;
+        IBOutlet UIButton *gobtn;
+        IBOutlet UIButton *rokubtn;
+        IBOutlet UIButton *nanabtn;
+        IBOutlet UIButton *hatibtn;
+        IBOutlet UIButton *kyubtn;
+        IBOutlet UIButton *equalbtn;
+        IBOutlet UIButton *plusbtn;
+        IBOutlet UIButton *minusbtn;
+        IBOutlet UIButton *shobtn;
+        IBOutlet UIButton *sekibtn;
+        IBOutlet UIButton *shosubtn;
+        IBOutlet UILabel *labellbl;
+
+        BOOL startInput;
+    }
+
+- (IBAction)clearbtn_down:(id)sender;
+- (IBAction)zerobtn_down:(id)sender;
+- (IBAction)itibtn_down:(id)sender;
+- (IBAction)nibtn_down:(id)sender;
+- (IBAction)sanbtn_down:(id)sender;
+- (IBAction)yonbtn_down:(id)sender;
+- (IBAction)gobtn_down:(id)sender;
+- (IBAction)rokubtn_down:(id)sender;
+- (IBAction)nanabtn_down:(id)sender;
+- (IBAction)hatibtn_down:(id)sender;
+- (IBAction)kyubtn_down:(id)sender;
+- (IBAction)equalbtn_down:(id)sender;
+- (IBAction)plusbtn_down:(id)sender;
+- (IBAction)minusbtn_down:(id)sender;
+- (IBAction)shobtn_down:(id)sender;
+- (IBAction)sekibtn_down:(id)sender;
+- (IBAction)shosubtn_down:(id)sender;
+
+
+
 
 
 @end
--- dentaku/ViewController.m
+++ dentaku/ViewController.m
@@ -20,6 +20,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    startInput = YES;
 }
 
 - (void)didReceiveMemoryWarning
@@ -28,4 +29,65 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)clearbtn_down:(id)sender {
+    labellbl.text = @"0";
+    startInput = YES;
+}
+
+- (IBAction)zerobtn_down:(id)sender {
+}
+
+- (IBAction)itibtn_down:(id)sender {
+    UIButton *b = (UIButton *)sender;
+    
+    if( startInput ){
+        if( b.tag == 0 )return;
+        labellbl.text = [NSString stringWithFormat:@"%d", b.tag];
+        startInput = NO;
+    } else {
+        labellbl.text = [NSString stringWithFormat:@"%@%d", labellbl.text, b.tag];
+    }
+}
+
+- (IBAction)nibtn_down:(id)sender {
+}
+
+- (IBAction)sanbtn_down:(id)sender {
+}
+
+- (IBAction)yonbtn_down:(id)sender {
+}
+
+- (IBAction)gobtn_down:(id)sender {
+}
+
+- (IBAction)rokubtn_down:(id)sender {
+}
+
+- (IBAction)nanabtn_down:(id)sender {
+}
+
+- (IBAction)hatibtn_down:(id)sender {
+}
+
+- (IBAction)kyubtn_down:(id)sender {
+}
+
+- (IBAction)equalbtn_down:(id)sender {
+}
+
+- (IBAction)plusbtn_down:(id)sender {
+}
+
+- (IBAction)minusbtn_down:(id)sender {
+}
+
+- (IBAction)shobtn_down:(id)sender {
+}
+
+- (IBAction)sekibtn_down:(id)sender {
+}
+
+- (IBAction)shosubtn_down:(id)sender {
+}
 @end
